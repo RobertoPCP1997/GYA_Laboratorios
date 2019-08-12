9be2d5bef6bb8f4f869a2b88ac7fc4289acf4adf
RobertoPCP1997 <roberto.carva97@gmail.com>
2019-08-12 12:11:09 -0500

    proyect_new

--- reactnotes/src/App.js
+++ reactnotes/src/App.js
@@ -1,48 +1,156 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Note from './Note/Note';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import firebase from './components/config/firebase';
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state ={
-      notes:[
-        {noteId: 1, noteContent: 'note 1'},
-        {noteId: 2, noteContent: 'note 2'},
-      ]
-    };
-  }
-  render () {
+import Login from './components/autentificacion/Login';
+import Header from './components/Header';
+import Marcador from './components/Marcador';
+
+import Laboratorios from './components/Laboratorios';
+import AgregarLaboratorio from './components/AgregarLaboratorio';
+import EditarLaboratorio from './components/EditarLaboratorio';
+import Laboratorio from './components/Laboratorio';
+
+import Horarios from './components/Horarios';
+import AgregarHorario from './components/AgregarHorario';
+import EditarHorario from './components/EditarHorario';
+import Horario from './components/Horario';
+
+
+
+
+function App() {
+
+  const [laboratorios, guardarLaboratorios] = useState([]);
+  const [horarios, guardarHorarios] = useState([]);
+
+  const [recargarLaboratorios, guardarRecargarLaboratorios] = useState(true);
+  const [autenticacion, guardarAutenticacion] = useState(false);
+
+
+  useEffect(() => {
+    if (recargarLaboratorios) {
+      firebase.firestore().collection('salas').onSnapshot((snapshot) => {
+        const datos = snapshot.docs.map((dato) => ({
+          id: dato.id,
+          ...dato.data()
+        }))
+        guardarLaboratorios(datos);
+      });
+      firebase.firestore().collection('horario').onSnapshot((snapshot) => {
+        const datos = snapshot.docs.map((dato) => ({
+          id: dato.id,
+          ...dato.data()
+        }))
+        guardarHorarios(datos);
+      });
+    }
+    //cambiar a false la recarga de los datos para que no se esté consultando a la API a cada rato
+    guardarRecargarLaboratorios(false);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        //El state se pone en true si el usuario esta logeado
+        return guardarAutenticacion(true);
+      } else {
+        //El state se pone en false si el usuario esta logeado
+        return guardarAutenticacion(false);
+      }
+    })
+  }, [recargarLaboratorios])
+
   return (
-    <div className="notesContainer">
-     
-     <div className="notesHeader">
-        <h1>REACT Y FIREBASE APP</h1>
-     </div>
-
-     <div className="notesBody">
-       <ul>{
-          this.state.notes.map(note => {
-            return (
-              <Note 
-              noteContent={note.noteContent}
-              noteId={note.noteId}
-              key={note.noteId}
+    <Router>
+      <Header />
+      <main className="container mt-5">
+        <Switch>
+          <Route exact path="/"
+            render={() => (
+              <Login
+                recargar={guardarRecargarLaboratorios}
               />
-            )
-          })
-        }
-        </ul>
-     </div>
+            )}
+          />
+          {/*aqui empieza las rutas de los laboratorisos*/}
+          <Route exact path="/laboratorios"
+            render={() => (
+              <Laboratorios
+                laboratorios={laboratorios}
+                guardarRecargarLaboratorios={guardarRecargarLaboratorios}
+                auth={autenticacion}
+              />
+            )}
+          />
+          <Route exact path="/nuevo-laboratorio"
+            render={() => (
+              <AgregarLaboratorio
+                guardarRecargarLaboratorios={guardarRecargarLaboratorios}
+                auth={autenticacion}
+              />
+            )} />
+          <Route exact path="/laboratorios/:id" component={Laboratorio} />
+          <Route exact path="/laboratorios/editar/:id"
+            render={props => {
+              // tomar el id del laboratorio
+              const idLaboratorio = props.match.params.id;
 
-     <div className="notesFooster">
-       
-     </div>
+              //el lab que se pasa al state
+              const laboratorio = laboratorios.filter(laboratorio => laboratorio.id ===
+                idLaboratorio);
+              return (
+                <EditarLaboratorio
+                  laboratorio={laboratorio[0]}
+                //guardarRecargarLaboratorios={guardarRecargarLaboratorios}
+                />
+              )
+            }}
+          />
+          {/*aqui empieza las rutas de los horarios*/}
+          <Route exact path="/horarios"
+            render={() => (
+              <Horarios
+                horarios={horarios}
+                auth={autenticacion}
+              />
+            )}
+          />
+          <Route exact path="/nuevo-horario"
+            render={() => (
+              <AgregarHorario
+                datos={laboratorios}
+                auth={autenticacion}
+              />
+            )} />
+          <Route exact path="/horarios/:id" component={Horario} />
+          <Route exact path="/horarios/editar/:id"
+            render={props => {
+              // tomar el id del horario
+              const idHorario = props.match.params.id;
 
-    </div>
-   );
-  } 
+              //el lab que se pasa al state
+              const horario = horarios.filter(horario => horario.id ===
+                idHorario);
+              return (
+                <EditarHorario
+                  horario={horario[0]}
+                //guardarRecargarLaboratorios={guardarRecargarLaboratorios}
+                />
+              )
+            }}
+          />
+          {/*aqui empieza la ruta del marcador*/}
+          <Route exact path="/nuevo-marcador"
+            render={() => (
+              <Marcador
+                auth={autenticacion}
+              />
+            )}
+          />
+        </Switch>
+      </main>
+      <p className="mt-4 p2 text-center">Todos los derechos reservados</p>
+    </Router>
+  );
 }
 
 export default App;
