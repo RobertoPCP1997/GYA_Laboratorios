d49b1f4dec3bb27f6b0dd48402cda448613dacd1
RobertoPCP1997 <roberto.carva97@gmail.com>
2019-08-14 11:31:40 -0500

    agregar los items al boton desplegable

--- sitemas_laboratorios/src/componentes/Header.js
+++ sitemas_laboratorios/src/componentes/Header.js
@@ -5,40 +5,36 @@ function Header({ history }) {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-2 mb-5">
-        
-                <div className="container-fluid">
-                    <div>
-                        <ul></ul>
-                    </div>
-                    
-                    <Link to="/laboratorios" className="navbar-brand">Sistema de Laboratorios | </Link>
-                   
-  
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <NavLink
-                                    to='/laboratorios'
-                                    className="nav-link"
-                                    activeClassName="active"
-                                >Laboratorios</NavLink>
+                            
+                            <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Sistema de Laboratorios |</a>
+                                     <div className="dropdown-divider"></div>
+                                     
+                                        <a className="dropdown-item" href="/laboratorios">Listado laboratorios</a>
+                                        <a className="dropdown-item" href="/nuevo-laboratorio">Nuevo laboratorio</a>
+                                    <div className="dropdown-divider"></div>
+                                        
+                                    <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Adminsitrar Cursos</a>
+                                     <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#">Listado</a>
+                                        <a class="dropdown-item" href="#">Curso nuevo</a>
+                                    <div class="dropdown-divider"></div>
+                                        <li className="nav-item">
+                                            <NavLink
+                                                to='/nuevo-laboratorio'
+                                                className="nav-link"
+                                                activeClassName="active"
+                                            >Nuevo Laboratorio</NavLink>
+                                        </li>
                             </li>
-                            <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                                
-                            </li>
-                            <li className="nav-item">
-                                <NavLink
-                                    to='/nuevo-laboratorio'
-                                    className="nav-link"
-                                    activeClassName="active"
-                                >Nuevo Laboratorio</NavLink>
-                            </li>
+                            
     
                         </ul>
                        
                     </div>
-                </div>
+                
         </nav>
     )
 }
