1cf1c505a4e25f981ad138928d272d8acc8af067
RobertoPCP1997 <roberto.carva97@gmail.com>
2019-08-14 12:50:55 -0500

    completo clasesD  CLASES

---
+++ sitemas_laboratorios/src/componentes/Horarios.js
@@ -0,0 +1,24 @@
+import React,{ Fragment} from 'react';
+import HorarioLista from './HorarioLista';
+
+function Horarios({horarios, guardarRecargarLaboratorios, auth}){
+    return (
+        <Fragment>
+            {auth ? (
+                <div>
+                    <h1 className="text-center">Horarios</h1>
+                    <ul className="list-group mt-5">
+                        {horarios.map(horario => (
+                            <HorarioLista
+                                key={horario.id}
+                                horario={horario}
+                                guardarRecargarLaboratorios={guardarRecargarLaboratorios}
+                            />
+                        ))}
+                    </ul>
+                </div>
+            ) : <h1 className="text-center">Página no disponible</h1>}
+        </Fragment>
+    )
+}
+export default Horarios;
